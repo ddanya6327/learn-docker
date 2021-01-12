a8bf77c78b821bcc89160e06410e279b030d0e41
ddanya6327 <69433959+ddanya6327@users.noreply.github.com>
2021-01-12 15:50:20 +0900

    add backend(express) and frontend(react)

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const db = require("./db");
+
+// express
+const app = express();
+
+// body parser
+app.use(bodyParser.json());
+
+// create table
+db.pool.query(
+  `CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`,
+  (err, results, fileds) => {
+    console.log("results", results);
+  }
+);
+
+// APIs
+// lists
+app.get("/api/values", function (req, res) {
+  db.pool.query("SELECT * FROM lists;", (err, results, fileds) => {
+    if (err) return res.status(500).send(err);
+    else return res.json(results);
+  });
+});
+
+// add
+app.post("/api/value", function (req, res, next) {
+  db.pool.query(
+    `INSERT INTO lists (value) VALUES("${req.body.value}"`,
+    (err, results, fileds) => {
+      if (err) return res.status(500).send(err);
+      else return res.json({ success: true, value: req.body.value });
+    }
+  );
+});
+
+app.listen(5000, () => {
+  console.log("App port 5000");
+});
